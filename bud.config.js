45580625457b8ac91679f68bf1fbf2e6ee2180f7
anam <anam.hossain@multidots.com>
2024-07-18 16:09:40 +0600

    replace space with tab

--- bud.config.js
+++ bud.config.js
@@ -7,74 +7,72 @@
  * @type {import('@roots/bud').Config}
  */
 export default async (app) => {
-  /**
-   * Application assets & entrypoints
-   *
-   * @see {@link https://bud.js.org/reference/bud.entry}
-   * @see {@link https://bud.js.org/reference/bud.assets}
-   */
-  app
-    .entry('app', ['@scripts/app', '@styles/app'])
-    .entry('editor', ['@scripts/editor', '@styles/editor'])
-    .assets(['images']);
+	/**
+	 * Application assets & entrypoints
+	 *
+	 * @see {@link https://bud.js.org/reference/bud.entry}
+	 * @see {@link https://bud.js.org/reference/bud.assets}
+	 */
+	app.entry('app', ['@scripts/app', '@styles/app'])
+		.entry('editor', ['@scripts/editor', '@styles/editor'])
+		.assets(['images']);
 
-  /**
-   * Set public path
-   *
-   * @see {@link https://bud.js.org/reference/bud.setPublicPath}
-   */
-  app.setPublicPath('/app/themes/sage/public/');
+	/**
+	 * Set public path
+	 *
+	 * @see {@link https://bud.js.org/reference/bud.setPublicPath}
+	 */
+	app.setPublicPath('/app/themes/sage/public/');
 
-  /**
-   * Development server settings
-   *
-   * @see {@link https://bud.js.org/reference/bud.setUrl}
-   * @see {@link https://bud.js.org/reference/bud.setProxyUrl}
-   * @see {@link https://bud.js.org/reference/bud.watch}
-   */
-  app
-    .setUrl('http://localhost:3000')
-    .setProxyUrl('https://anamstarter.local')
-    .watch(['resources/views', 'app']);
+	/**
+	 * Development server settings
+	 *
+	 * @see {@link https://bud.js.org/reference/bud.setUrl}
+	 * @see {@link https://bud.js.org/reference/bud.setProxyUrl}
+	 * @see {@link https://bud.js.org/reference/bud.watch}
+	 */
+	app.setUrl('http://localhost:3000')
+		.setProxyUrl('https://anamstarter.local')
+		.watch(['resources/views', 'app']);
 
-  /**
-   * Generate WordPress `theme.json`
-   *
-   * @note This overwrites `theme.json` on every build.
-   *
-   * @see {@link https://bud.js.org/extensions/sage/theme.json}
-   * @see {@link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-json}
-   */
-  app.wpjson
-    .setSettings({
-      background: {
-        backgroundImage: true,
-      },
-      color: {
-        custom: false,
-        customDuotone: false,
-        customGradient: false,
-        defaultDuotone: false,
-        defaultGradients: false,
-        defaultPalette: false,
-        duotone: [],
-      },
-      custom: {
-        spacing: {},
-        typography: {
-          'font-size': {},
-          'line-height': {},
-        },
-      },
-      spacing: {
-        padding: true,
-        units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
-      },
-      typography: {
-        customFontSize: false,
-      },
-    })
-    .useTailwindColors()
-    .useTailwindFontFamily()
-    .useTailwindFontSize();
+	/**
+	 * Generate WordPress `theme.json`
+	 *
+	 * @note This overwrites `theme.json` on every build.
+	 *
+	 * @see {@link https://bud.js.org/extensions/sage/theme.json}
+	 * @see {@link https://developer.wordpress.org/block-editor/how-to-guides/themes/theme-json}
+	 */
+	app.wpjson
+		.setSettings({
+			background: {
+				backgroundImage: true,
+			},
+			color: {
+				custom: false,
+				customDuotone: false,
+				customGradient: false,
+				defaultDuotone: false,
+				defaultGradients: false,
+				defaultPalette: false,
+				duotone: [],
+			},
+			custom: {
+				spacing: {},
+				typography: {
+					'font-size': {},
+					'line-height': {},
+				},
+			},
+			spacing: {
+				padding: true,
+				units: ['px', '%', 'em', 'rem', 'vw', 'vh'],
+			},
+			typography: {
+				customFontSize: false,
+			},
+		})
+		.useTailwindColors()
+		.useTailwindFontFamily()
+		.useTailwindFontSize();
 };
