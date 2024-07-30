9b2e9cff5e254db63e4bf8d44ff21e17418f0539
anam <anam.hossain@multidots.com>
2024-07-30 12:58:39 +0600

    [clean] remove commented code

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
-
 @section('content')
-	{{-- shortcode --}}
-	{{-- {!! do_shortcode('[custom_field_data]') !!} --}}
-
 	@while(have_posts()) @php(the_post())
 		@include('partials.page-header')
 		@includeFirst(['partials.content-page', 'partials.content'])
